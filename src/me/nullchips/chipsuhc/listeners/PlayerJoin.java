9ef8d3f4ce42a2db0d922e58b5300ddc6c663ae2
NullChips <tommyjhns@gmail.com>
2017-02-14 16:05:53 +0000

    Added Event handlers. Listeners now work.

--- src/me/nullchips/chipsuhc/listeners/PlayerJoin.java
+++ src/me/nullchips/chipsuhc/listeners/PlayerJoin.java
@@ -3,6 +3,7 @@ package me.nullchips.chipsuhc.listeners;
 import me.nullchips.chipsuhc.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -13,9 +14,10 @@ public class PlayerJoin implements Listener {
 
     ChatUtils cu = ChatUtils.getInstance();
 
+    @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         e.setJoinMessage("");
-        cu.broadcast(ChatColor.AQUA + e.getPlayer().getName() + ChatColor.WHITE + " has joined the game.");
+        cu.broadcast(ChatColor.GREEN + e.getPlayer().getName() + ChatColor.WHITE + " has joined the game.");
     }
 
 }
--- src/me/nullchips/chipsuhc/listeners/PlayerLeave.java
+++ src/me/nullchips/chipsuhc/listeners/PlayerLeave.java
@@ -2,6 +2,7 @@ package me.nullchips.chipsuhc.listeners;
 
 import me.nullchips.chipsuhc.utils.ChatUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -12,9 +13,10 @@ public class PlayerLeave implements Listener {
 
     ChatUtils cu = ChatUtils.getInstance();
 
+    @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e) {
         e.setQuitMessage("");
-        cu.broadcast(ChatColor.AQUA + e.getPlayer().getName() + ChatColor.WHITE + " has left the game.");
+        cu.broadcast(ChatColor.GREEN + e.getPlayer().getName() + ChatColor.WHITE + " has left the game.");
 
         //TODO Add time-out when in game.
 
